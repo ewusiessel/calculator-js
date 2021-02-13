ba4aee7574cc0e009d796dfb930326f61a54f6ce
Ekow <ewusiessel@hotmail.com>
2021-02-13 03:35:36 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM loaded");
+
+  const numericButtons = document.getElementsByClassName("numericButton");
+  console.log(numericButtons);
+});
