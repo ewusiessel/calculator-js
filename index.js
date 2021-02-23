f0936c5f21f6059f7fc0dc8a30a2412e783e0b4b
Ekow <ewusiessel@hotmail.com>
2021-02-23 02:21:49 +0000

    add event listeners

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 window.addEventListener("DOMContentLoaded", function () {
   console.log("DOM loaded");
 
+  const numericButtonPressed = function (event) {
+    console.log("number pressed", numberPressed);
+    console.log("number pressed", event);
+  };
+
   const numericButtons = document.getElementsByClassName("numericButton");
-  console.log(numericButtons);
+  const functionButtons = document.getElementsByClassName("functionButton");
+
+  for (let button of numericButtons) {
+    button.addEventListener("click", numericButtonPressed);
+  }
+
+  for (let button of functionButtons) {
+    button.addEventListener("click", functionButtonPressed);
+  }
 });
